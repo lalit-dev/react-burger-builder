cbe90221d9a61794397db20f23dee1edbaf18822
lalit-dev <lalityadav.mail@gmail.com>
2020-01-29 16:01:10 +0530

    toolbar-header

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 import classes from "./Layout.css"
 
-import Aux from '../../hoc/auxiliary'
+import Aux from '../../hoc/auxiliary';
+import Toolbar from "./../../components/Navigation/Toolbar/Toolbar"; 
 
 const layout = (props) => {
     return (
         <Aux>
-            <div>toolbar, sidebar, backdrop</div>
+            <Toolbar />
             <main className={classes.content}>
                 {props.children}
             </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,20 @@
+import  React from "react";
+
+import classes from "./Toolbar.css";
+import Logo from "./../../Logo/Logo";
+import NavigationItems from "./../NavigationItems/NavigationItems";
+
+const toolbar = (props) => {
+    return (
+        <header className = {classes.Toolbar}>
+            <div>Menu</div>
+            <Logo />
+            <nav>
+                <NavigationItems />
+            </nav>
+
+        </header>
+    )
+}
+
+export default toolbar
